510aa17177a7497d221d4e9f7202acfe873db736
Marvin Mednick <mmednick@gmail.com>
2022-08-13 20:40:02 -0700

    struct working

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,39 @@
 
 
 struct BellmanData {
-    array : Vec::<Vec<u32>>,``
-    num_vertex: u32,
-    num_iterations: u32
+    array : Vec::<Vec<u32>>,
+    num_vertex: usize,
+    num_iterations: usize
 
 }
 
 impl BellmanData {
 
-    pub fn new(num_vertex: u32, num_iterations: u32) -> Self {
-        let array = Vec::<Vec<u32>>::new();
-        // TODO -- Initialize
+    pub fn new(num_vertex: usize, num_iterations: usize) -> BellmanData {
+        let mut array = Vec::<Vec<u32>>::new();
+        for y in 0..num_iterations  {
+            let mut row = Vec::<u32>::new();
+            for x in 0..num_vertex {
+                let value = y*num_vertex + x;
+                row.push(value.try_into().unwrap());
+            }
+            array.push(row);
+        }
+
         BellmanData { array: array, num_vertex: num_vertex, num_iterations: num_iterations }
     }
 
-    pub fn get(vertex, iteration) -> u32 {
+    pub fn get(&self,vertex: usize, iteration: usize) -> u32 {
+        self.array[iteration][vertex]
 
     }
 
-    pub fn set(vertex, iteration, value)  {
+    pub fn get_row_iter(&self) -> std::slice::Iter<'_, Vec<u32>>{
+        self.array.iter()
+    }
+
+    pub fn set(&mut self, vertex : usize, iteration : usize , value : u32)  {
+        self.array[iteration][vertex] = value;
 
     }
 
@@ -29,13 +43,15 @@ impl BellmanData {
 fn main() { 
     let vertexes = 7;
     let iterations = vertexes-1;
+
+    let data = BellmanData::new(vertexes, iterations);
    
     let mut xy_array = Vec::<Vec<u32>>::new();
     for y in 0..iterations  {
         let mut row = Vec::<u32>::new();
         for x in 0..vertexes {
             let value = y*vertexes + x;
-            row.push(value);
+            row.push(value.try_into().unwrap());
 
         }
         xy_array.push(row);
@@ -50,4 +66,17 @@ fn main() {
         println!("Iteration {:2} :    {}", count,row_format);
         count += 1
     }
+
+    let mut count = 0;
+    for row in data.get_row_iter() {
+        let row_format : String = row.iter().map(|val| format!("{:2} ",val)).collect();
+        println!("Iteration {:2} :    {}", count,row_format);
+        count += 1
+    }
+
+    println!("Iteration 1,3 -> {}", data.get(3,1));
+    println!("Iteration 4,1 -> {}", data.get(1,4));
+
+
+
 }
