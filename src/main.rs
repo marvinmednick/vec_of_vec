741b21a4172e1995f890e989b6b4ca7ad93b4e79
Marvin Mednick <mmednick@gmail.com>
2022-08-23 13:50:29 -0700

    Converted to generic x and y

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,35 @@
 use std::fmt::format;
 
 #[derive(Debug)]
-struct BellmanData<T> {
+struct VecofVec<T> {
     array : Vec::<Vec<Option<T>>>,
-    num_vertex: usize,
-    num_iterations: usize
+    x: usize,
+    y: usize
 
 }
 
-impl<T:std::fmt::Debug+Clone+std::fmt::Display> BellmanData<T> {
+impl<T:std::fmt::Debug+Clone+std::fmt::Display> VecofVec<T> {
 
-    pub fn new(num_vertex: usize, num_iterations: usize) -> BellmanData<T> {
+    pub fn new(x: usize, y: usize) -> VecofVec<T> {
         let mut array = Vec::<Vec<Option<T>>>::new();
-        for y in 0..num_iterations  {
-            let mut row = Vec::<Option<T>>::new();
-            for x in 0..num_vertex {
-                row.push(None);
+        for row in 0..y  {
+            let mut row_data = Vec::<Option<T>>::new();
+            for col in 0..x {
+                row_data.push(None);
             }
-            array.push(row);
+            array.push(row_data);
         }
 
-        BellmanData { array: array, num_vertex: num_vertex, num_iterations: num_iterations }
+        VecofVec { array: array, x: x, y: y }
     }
 
-    pub fn get(&self,vertex: usize, iteration: usize) -> Option<T> {
-        self.array[iteration][vertex].clone()
+    pub fn get(&self,x: usize, y: usize) -> Option<T> {
+        self.array[y][x].clone()
     }
 
-    pub fn get_string(&self,vertex: usize, iteration: usize) ->  String{
-        match &self.array[iteration][vertex] {
-            Some(x) => format!("{}",x),
+    pub fn get_string(&self,x: usize, y: usize) ->  String{
+        match &self.array[y][x] {
+            Some(val) => format!("{}",val),
             None =>   format!("{}","N"),
         }
     }
@@ -40,8 +40,8 @@ impl<T:std::fmt::Debug+Clone+std::fmt::Display> BellmanData<T> {
         self.array.iter()
     }
 
-    pub fn set(&mut self, vertex : usize, iteration : usize , value : Option<T>)  {
-        self.array[iteration][vertex] = value;
+    pub fn set(&mut self, x : usize, y : usize , value : T)  {
+        self.array[y][x] = Some(value);
 
     }
 
@@ -52,44 +52,25 @@ fn main() {
     let vertexes = 7;
     let iterations = vertexes-1;
 
-    let data : BellmanData<u32> = BellmanData::new(vertexes, iterations);
+    let mut data : VecofVec<u32> = VecofVec::new(vertexes, iterations);
+
+
+    data.set(3,0,44);
    
-    /*
-    let mut xy_array = Vec::<Vec<u32>>::new();
-    for y in 0..iterations  {
-        let mut row = Vec::<u32>::new();
-        for x in 0..vertexes {
-            let value = y*vertexes + x;
-            row.push(value.try_into().unwrap());
-
-        }
-        xy_array.push(row);
-
-    }
-
-
-    let mut count = 0;
-    println!("{:17} {:2}","Vertex",1);
-    for row in xy_array {
-        let row_format : String = row.iter().map(|val| format!("{:2} ",val)).collect();
-        println!("Iteration {:2} :    {}", count,row_format);
-        count += 1
-    }
-
-    */
     let mut count = 0;
     let header : String = (0..vertexes).map(|val| format!("{:2} ",val)).collect();
     println!("{:17} {}","Vertex",header);
     for row in data.get_row_iter() {
         let row_format : String = row.iter().map(|val| { 
             match val {
-                Some(x) => format!("{:2} ",val.unwrap()),
-                None    => format!("{:2} ","N"),
+                Some(x) => format!("{:>2} ",val.unwrap()),
+                None    => format!("{:>2} ","N"),
             }}).collect();
         println!("Iteration {:2} :    {}", count,row_format);
         count += 1
     }
 
+    println!("Iter 0,Vertex 3 -> {}", data.get_string(3,0));
     println!("Iter 1,Vertex 3 -> {}", data.get_string(3,1));
 
     println!("Iter 4,Vertex 1 -> {}", data.get_string(1,4));
