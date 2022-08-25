2b8b8884f701b376c4a97523ecfab22b449dea7c
Marvin Mednick <mmednick@gmail.com>
2022-08-24 20:14:26 -0700

    Added flattened array

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 use std::fmt::format;
 
 #[derive(Debug)]
-struct VecOfVec<T> {
+pub struct VecOfVec<T> {
     array : Vec::<Vec<Option<T>>>,
     width: usize,
     height: usize
@@ -59,6 +59,73 @@ impl<T:std::fmt::Debug+Clone+std::fmt::Display> VecOfVec<T> {
 }
 
 
+#[derive(Debug)]
+pub struct FlattendArray<T> {
+    array : Vec::<Option<T>>,
+    width: usize,
+    height: usize
+
+}
+
+impl<T:std::fmt::Debug+Clone+std::fmt::Display> FlattendArray<T> {
+
+    pub fn new(width: usize, height: usize) -> FlattendArray<T> {
+        let mut array = Vec::<Option<T>>::new();
+        let total_size = width * height;
+        for index in 0..total_size  {
+            array.push(None);
+        }
+
+        FlattendArray { array: array, width: width, height: height }
+    }
+
+    pub fn get(&self,x: usize, y: usize) -> Option<T> {
+        if x < self.width && y < self.height {
+            let index = y * self.width + x;
+            self.array[index].clone()
+        }
+        else {
+            None
+        }
+    }
+
+    pub fn get_string(&self,x: usize, y: usize) ->  String{
+        if x < self.width && y < self.height {
+            let index = y * self.width + x;
+            match &self.array[index] {
+                Some(val) => format!("{}",val),
+                None =>   format!("{}","N"),
+            }
+        }
+        else {
+                format!("{}","N")
+        }
+    }
+
+/*
+    pub fn get_row_iter(&self) -> std::slice::Iter<Vec<Option<T>>> {
+        # need
+        self.array.iter()
+    }
+*/
+
+    pub fn set(&mut self, x : usize, y : usize , value : T)  {
+        if x < self.width && y < self.height {
+            let index = y * self.width + x;
+            self.array[index] = Some(value);
+        }
+    }
+
+    pub fn unset(&mut self, x : usize, y : usize )  {
+        if x < self.width && y < self.height {
+            let index = y * self.width + x;
+            self.array[index] = None;
+        }
+    }
+
+}
+
+/*
 fn main() { 
     let vertexes = 7;
     let iterations = vertexes-1;
@@ -90,6 +157,7 @@ fn main() {
 
 
 }
+*/
 
 
 #[cfg(test)]
@@ -101,6 +169,33 @@ mod vec_of_vec_test {
     fn test_init() {
         let size = 8; 
         let mut data : VecOfVec<u32> = VecOfVec::new(size, size);
+
+
+        data.set(1,2,12);
+        data.set(2,3,23);
+        data.set(3,4,34);
+        data.set(6,7,67);
+        data.set(8,8,88);
+        for i in 0..size {
+            assert_eq!(data.get(i,i), None)
+        }
+        assert_eq!(data.get(1,2),Some(12));
+        assert_eq!(data.get(2,3),Some(23));
+        assert_eq!(data.get(3,4),Some(34));
+        assert_eq!(data.get(6,7),Some(67));
+        assert_eq!(data.get(8,8),None);
+    }
+}
+
+#[cfg(test)]
+mod flattened_array_test {
+
+    use crate::FlattendArray;
+
+    #[test]
+    fn test_init() {
+        let size = 8; 
+        let mut data : FlattendArray<u32> = FlattendArray::new(size, size);
 
 
         data.set(1,2,12);
