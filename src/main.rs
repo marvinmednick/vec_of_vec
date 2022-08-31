cb3399eac43948c1810e0ab38404ab80f7a78ed5
Marvin Mednick <mmednick@gmail.com>
2022-08-31 10:59:53 -0700

    Fix up returning data lost with move and merge

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
+use vec_of_vec::{VecOfVec,FlattendArray};
 
 fn main() { 
     let vertexes = 7;
     let iterations = vertexes-1;
+
+    let mut data : VecOfVec<u32> = VecOfVec::new(vertexes, iterations);
+
+
+    data.set(3,0,44);
+   
+    let mut count = 0;
+    let header : String = (0..vertexes).map(|val| format!("{:2} ",val)).collect();
+    println!("{:17} {}","Vertex",header);
+    for row in data.get_row_iter() {
+        let row_format : String = row.iter().map(|val| { 
+            match val {
+                Some(x) => format!("{:>2} ",val.unwrap()),
+                None    => format!("{:>2} ","N"),
+            }}).collect();
+        println!("Iteration {:2} :    {}", count,row_format);
+        count += 1
+    }
+
+    println!("Iter 0,Vertex 3 -> {}", data.get_string(3,0));
+    println!("Iter 1,Vertex 3 -> {}", data.get_string(3,1));
+
+    println!("Iter 4,Vertex 1 -> {}", data.get_string(1,4));
+    println!("Iter 6,Vertex 5 -> {}", data.get_string(6,5));
 
 
     println!("\n--------- Flattened Array ------- \n");
